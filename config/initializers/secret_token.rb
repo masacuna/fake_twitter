126c4c889f6bfc952ac9a2e10f7d20e91c066c3f
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-03-31 09:13:49 -0500

    Initial commit of a new rails project will be fake twitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FakeTwitter::Application.config.secret_key_base = '69ba9256a2ceb21644e5f1ced9eae9714d99731bc4f872ee126e1506d783f079e676ed1e494613a233611366933638ce273b2d0a16733f11803e0557d19bcd13'
