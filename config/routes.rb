ede82cd5d29e06986c02085fe3437fd38f2e4ba0
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-03-31 09:38:31 -0500

    added controller tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FakeTwitter::Application.routes.draw do
+  
+
+  root 'tweets#index'
+
+  #post 'tweets', to: 'tweets#create' 
+
+  resource :tweets , :only => [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
